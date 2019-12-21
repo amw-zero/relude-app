1c3ac155f61381d728e3bbe3ec9f3b333dba23bd
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-12-20 22:30:04 -0500

    Working async IO

--- src/Route.bs.js
+++ src/Route.bs.js
@@ -1,12 +1,75 @@
 'use strict';
 
+var Block = require("bs-platform/lib/js/block.js");
+var Curry = require("bs-platform/lib/js/curry.js");
+var Axios = require("axios");
 var React = require("react");
+var Relude_IO = require("relude/src/Relude_IO.bs.js");
+var ReludeReact_Render = require("relude-reason-react/src/ReludeReact_Render.bs.js");
+var Relude_AsyncResult = require("relude/src/Relude_AsyncResult.bs.js");
+var ReludeReact_Reducer = require("relude-reason-react/src/ReludeReact_Reducer.bs.js");
+var Caml_builtin_exceptions = require("bs-platform/lib/js/caml_builtin_exceptions.js");
+
+var api = "http://localhost:3000/promise";
+
+var initialState = {
+  data: Relude_AsyncResult.init
+};
+
+function reducer(state, action) {
+  if (typeof action === "number") {
+    return /* UpdateWithIO */Block.__(5, [
+              {
+                data: Relude_AsyncResult.toBusy(state.data)
+              },
+              Relude_IO.async((function (onDone) {
+                      Axios.get(api).then((function (r) {
+                              return Promise.resolve(Curry._1(onDone, /* Ok */Block.__(0, [/* FetchRouteSuccess */Block.__(0, [r.data])])));
+                            }));
+                      return /* () */0;
+                    }))
+            ]);
+  } else if (action.tag) {
+    throw [
+          Caml_builtin_exceptions.match_failure,
+          /* tuple */[
+            "Route.re",
+            16,
+            33
+          ]
+        ];
+  } else {
+    return /* Update */Block.__(0, [{
+                data: Relude_AsyncResult.completeOk(action[0])
+              }]);
+  }
+}
+
+function renderData(d) {
+  return ReludeReact_Render.asyncResultByValueLazy((function (param) {
+                return React.createElement("p", undefined, "Loading");
+              }), (function (data) {
+                return React.createElement("p", undefined, "Data");
+              }), (function (error) {
+                return React.createElement("p", undefined, "Error");
+              }), d);
+}
 
 function Route(Props) {
-  return React.createElement("p", undefined, "test");
+  var match = ReludeReact_Reducer.useReducer(reducer, initialState);
+  var dispatch = match[1];
+  return React.createElement(React.Fragment, undefined, renderData(match[0].data), React.createElement("button", {
+                  onClick: (function (param) {
+                      return Curry._1(dispatch, /* FetchRoute */0);
+                    })
+                }, "fetch route"));
 }
 
 var make = Route;
 
+exports.api = api;
+exports.initialState = initialState;
+exports.reducer = reducer;
+exports.renderData = renderData;
 exports.make = make;
-/* react Not a pure module */
+/* axios Not a pure module */
--- src/Route.re
+++ src/Route.re
@@ -1,5 +1,51 @@
+open Relude.Globals;
+open ReludeReact;
+open Axios;
+
+let api = "http://localhost:3000/promise";
+
+type state = {data: AsyncResult.t(string, string)};
+
+let initialState = {data: AsyncResult.init};
+
+type action =
+  | FetchRoute
+  | FetchRouteSuccess(string)
+  | FetchRouteError(string);
+
+let reducer = (state, action) => {
+  switch (action) {
+  | FetchRoute =>
+    Reducer.UpdateWithIO(
+      {data: state.data |> AsyncResult.toBusy},
+      IO.async(onDone =>
+        Js.Promise.(
+          Axios.get(api)
+          |> then_(r => {onDone(Ok(FetchRouteSuccess(r##data))) |> resolve})
+        )
+        |> ignore
+      ),
+    )
+  | FetchRouteSuccess(s) => Reducer.Update({data: AsyncResult.completeOk(s)})
+  };
+};
+
+let renderData = d =>
+  d
+  |> ReludeReact.Render.asyncResultByValueLazy(
+       _ => <p> {React.string("Loading")} </p>,
+       data => <p> {React.string("Data")} </p>,
+       error => <p> {React.string("Error")} </p>,
+     );
 
 [@react.component]
 let make = () => {
-  <p>{React.string("test")}</p>;
+  let (state, dispatch) = Reducer.useReducer(reducer, initialState);
+
+  <>
+    {renderData(state.data)}
+    <button onClick={_ => dispatch(FetchRoute)}>
+      {React.string("fetch route")}
+    </button>
+  </>;
 };
